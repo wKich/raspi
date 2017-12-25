7b6f4d7be79b8f49cc7ca1c7149068a30e3322d2
wKich <nflazzer@gmail.com>
2017-12-25 23:03:47 +0500

    Add cleaner output

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@
 import { Gpio } from 'pigpio'
 import dht11 from './dht11'
 
+function clear() {
+  // The first code `\u001B[2J` instructs the terminal to clear itself,
+  // and the second one `\u001B[0;0f` forces the cursor back to position 0,0.
+  process.stdout.write('\u001B[2J\u001B[0;0f')
+}
+
 dht11(
   new Gpio(4, { alert: true }),
   (humidity, temperature) => {
+    clear()
+    console.log(`Время: ${new Date().toLocaleTimeString()}`)
     console.log(`Влажность: ${humidity}%`)
     console.log(`Температура: ${temperature} *C`)
   }
